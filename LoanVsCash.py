d010366ccb9cf12e47573d544589dd885b01bc2b
sayboltm <sayboltm@users.noreply.github.com>
2017-11-13 08:23:45 -0500

    New project! should be easy right

---
+++ LoanVsCash.py
@@ -0,0 +1,12 @@
+''' Some stuff to figure out if moar efficient to do a loan or save up and do
+cash '''
+
+import numpy as np
+
+age = np.arange(18,60,1)
+
+money_cashpath = 0
+money_loanpath = 70000
+
+
+
